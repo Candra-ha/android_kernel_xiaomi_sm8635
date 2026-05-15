48b189e9dffdea5f340227683814a17b83fa7902
Candra-ha <situshalal1@gmail.com>
2026-05-15 09:14:08 +0700

    Remove unused bio functions and kcompressd handling
    Removed unused bio handling functions and related code for kcompressd.

--- drivers/block/zram/zram_drv.c
+++ drivers/block/zram/zram_drv.c
@@ -41,7 +41,6 @@
 #include <linux/mm_inline.h>
 #include <linux/numa.h>
 
-#include "kcompressd.h"
 #include "zram_drv.h"
 
 static DEFINE_IDR(zram_index_idr);
@@ -1905,21 +1904,6 @@ static void __zram_make_request(struct zram *zram, struct bio *bio)
 	bio_endio(bio);
 }
 
-static void zram_bio_read(struct zram *zram, struct bio *bio);
-
-static void zram_bio_write(struct zram *zram, struct bio *bio);
-
-static void zram_bio_discard_c(struct zram *zram, struct bio *bio);
-
-#if IS_ENABLED(CONFIG_KCOMPRESSD)
-static void zram_bio_write_callback(void *mem, struct bio *bio)
-{
-	struct zram *zram = (struct zram *)mem;
-
-	zram_bio_write(zram, bio);
-}
-#endif
-
 /*
  * Handler function for all zram I/O requests.
  */
@@ -1932,26 +1916,6 @@ static void zram_submit_bio(struct bio *bio)
 		atomic64_inc(&zram->stats.invalid_io);
 		bio_io_error(bio);
 		return;
-	}
-
-	switch (bio_op(bio)) {
-	case REQ_OP_READ:
-		zram_bio_read(zram, bio);
-		break;
-	case REQ_OP_WRITE:
-#if IS_ENABLED(CONFIG_KCOMPRESSD)
-		if (kcompressd_enabled() && !schedule_bio_write(zram, bio, zram_bio_write_callback))
-			break;
-#endif
-		zram_bio_write(zram, bio);
-		break;
-	case REQ_OP_DISCARD:
-	case REQ_OP_WRITE_ZEROES:
-		zram_bio_discard_c(zram, bio);
-		break;
-	default:
-		WARN_ON_ONCE(1);
-		bio_endio(bio);
 	}
 
 	__zram_make_request(zram, bio);
