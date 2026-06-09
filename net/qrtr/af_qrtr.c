c7f0f87c5a7fdb47c8e30a7b787e3a3798e240eb
Anggara <situshalal1@gmail.com>
2026-06-09 19:18:27 +0000

    fix some error

--- net/qrtr/af_qrtr.c
+++ net/qrtr/af_qrtr.c
@@ -162,7 +162,6 @@ static struct work_struct qrtr_backup_work;
  * @nid: node id
  * @net_id: network cluster identifer
  * @qrtr_tx_flow: tree of qrtr_tx_flow, keyed by node << 32 | port
- * @qrtr_tx_flow: xarray of qrtr_tx_flow, keyed by node << 32 | port
  * @qrtr_tx_lock: lock for qrtr_tx_flow inserts
  * @hello_sent: hello packet sent to endpoint
  * @hello_rcvd: hello packet received from endpoint
@@ -184,7 +183,7 @@ struct qrtr_node {
 	atomic_t hello_sent;
 	atomic_t hello_rcvd;
 
-	struct xarray qrtr_tx_flow;
+	struct radix_tree_root qrtr_tx_flow;
 	struct mutex qrtr_tx_lock; /* for qrtr_tx_flow */
 
 	struct sk_buff_head rx_queue;
@@ -430,7 +429,6 @@ static void __qrtr_node_release(struct kref *kref)
 	struct qrtr_tx_flow *flow;
 	unsigned long flags;
 	void __rcu **slot;
-	unsigned long index;
 
 	spin_lock_irqsave(&qrtr_nodes_lock, flags);
 	/* If the node is a bridge for other nodes, there are possibly
@@ -452,9 +450,18 @@ static void __qrtr_node_release(struct kref *kref)
 	xa_destroy(&node->no_wake_svc);
 
 	/* Free tx flow counters */
-	xa_for_each(&node->qrtr_tx_flow, index, flow)
+	mutex_lock(&node->qrtr_tx_lock);
+	radix_tree_for_each_slot(slot, &node->qrtr_tx_flow, &iter, 0) {
+		flow = *slot;
+		list_for_each_entry_safe(waiter, temp, &flow->waiters, node) {
+			list_del(&waiter->node);
+			sock_put(waiter->sk);
+			kfree(waiter);
+		}
+		radix_tree_iter_delete(&node->qrtr_tx_flow, &iter, slot);
 		kfree(flow);
-	xa_destroy(&node->qrtr_tx_flow);
+	}
+	mutex_unlock(&node->qrtr_tx_lock);
 
 	kfree(node);
 }
@@ -522,12 +529,6 @@ static void qrtr_tx_resume(struct qrtr_node *node, struct sk_buff *skb)
 		}
 		sock_put(waiter->sk);
 		kfree(waiter);
-	flow = xa_load(&node->qrtr_tx_flow, key);
-	if (flow) {
-		spin_lock(&flow->resume_tx.lock);
-		flow->pending = 0;
-		spin_unlock(&flow->resume_tx.lock);
-		wake_up_interruptible_all(&flow->resume_tx);
 	}
 	spin_unlock_irqrestore(&flow->lock, flags);
 
@@ -567,15 +568,14 @@ static int qrtr_tx_wait(struct qrtr_node *node, struct sockaddr_qrtr *to,
 	timeo = sock_sndtimeo(sk, flags & MSG_DONTWAIT);
 
 	mutex_lock(&node->qrtr_tx_lock);
-	flow = xa_load(&node->qrtr_tx_flow, key);
+	flow = radix_tree_lookup(&node->qrtr_tx_flow, key);
 	if (!flow) {
 		flow = kzalloc(sizeof(*flow), GFP_KERNEL);
 		if (flow) {
 			INIT_LIST_HEAD(&flow->waiters);
 			init_waitqueue_head(&flow->resume_tx);
 			spin_lock_init(&flow->lock);
-			if (xa_err(xa_store(&node->qrtr_tx_flow, key, flow,
-					    GFP_KERNEL))) {
+			if (radix_tree_insert(&node->qrtr_tx_flow, key, flow)) {
 				kfree(flow);
 				flow = NULL;
 			}
@@ -650,7 +650,9 @@ static void qrtr_tx_flow_failed(struct qrtr_node *node, int dest_node,
 	unsigned long key = (u64)dest_node << 32 | dest_port;
 	struct qrtr_tx_flow *flow;
 
-	flow = xa_load(&node->qrtr_tx_flow, key);
+	mutex_lock(&node->qrtr_tx_lock);
+	flow = radix_tree_lookup(&node->qrtr_tx_flow, key);
+	mutex_unlock(&node->qrtr_tx_lock);
 	if (flow) {
 		spin_lock_irq(&flow->lock);
 		flow->tx_failed = 1;
@@ -1427,7 +1429,7 @@ int qrtr_endpoint_register(struct qrtr_endpoint *ep, unsigned int net_id,
 		}
 	}
 
-	xa_init(&node->qrtr_tx_flow);
+	INIT_RADIX_TREE(&node->qrtr_tx_flow, GFP_KERNEL);
 	mutex_init(&node->qrtr_tx_lock);
 
 	qrtr_node_assign(node, node->nid);
@@ -1515,7 +1517,6 @@ void qrtr_endpoint_unregister(struct qrtr_endpoint *ep)
 	struct qrtr_tx_flow *flow;
 	struct sk_buff *skb;
 	unsigned long flags;
-	unsigned long index;
 	void __rcu **slot;
 
 	mutex_lock(&node->ep_lock);
@@ -1541,8 +1542,10 @@ void qrtr_endpoint_unregister(struct qrtr_endpoint *ep)
 
 	/* Wake up any transmitters waiting for resume-tx from the node */
 	mutex_lock(&node->qrtr_tx_lock);
-	xa_for_each(&node->qrtr_tx_flow, index, flow)
+	radix_tree_for_each_slot(slot, &node->qrtr_tx_flow, &iter, 0) {
+		flow = *slot;
 		wake_up_interruptible_all(&flow->resume_tx);
+	}
 	mutex_unlock(&node->qrtr_tx_lock);
 
 	qrtr_node_release(node);
